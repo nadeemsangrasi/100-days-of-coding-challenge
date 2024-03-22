7d35c614448c074b2b6dc9028e6083a613034c0c
Nadeem Sangrasi <140242307+nadeemsangrasi@users.noreply.github.com>
2024-03-22 16:52:38 +0500

    Add files via upload

---
+++ Day-21.ts
@@ -0,0 +1,41 @@
+// Question 61: Making Enums for Vehicles: Let's create categories for vehicles like cars, trucks, and motorcycles using enums, and show one example.
+// Question 62: Making a Student Template: Create a blueprint for student information, including their name, age, and the classes they're taking, and then fill in this blueprint with an example student.
+// Question 63: Shape Shifter: Write a program that can describe either a circle or a rectangle using a special type alias, including properties unique to each shape.
+
+
+enum VehicleType {
+    Car,
+    Truck,
+    Motorcycle
+}
+console.log(VehicleType.Car); 
+
+interface Student {
+    name: string;
+    age: number;
+    courses: string[];
+}
+let student: Student = {
+    name: "Alice",
+    age: 22,
+    courses: ["Math", "Science", "History"]
+};
+console.log(student);
+type Shape = {
+    kind: "circle" | "rectangle";
+    radius?: number; 
+    width?: number; 
+    height?: number; 
+};
+
+let circle: Shape = {
+    kind: "circle",
+    radius: 5
+};
+let rectangle: Shape = {
+    kind: "rectangle",
+    width: 10,
+    height: 20
+};
+console.log(circle); 
+console.log(rectangle); 
